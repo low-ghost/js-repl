5a2c87a0abb58854bf82b9b6c3c90e7b3e9b48a7
mbagwell <mbagwell@excelmicro.com>
2016-08-15 18:40:20 -0400

    init pg success

--- cw.js
+++ cw.js
@@ -1,21 +1,63 @@
 const lo = require('lodash');
 const fp = require('lodash/fp');
+const connectwiseApi = require('lib-connectwise');
+const postgresApi = require('lib-db-postgres').default;
+const redisApi = require('redis');
+const fs = require('fs');
 
-const secrets = require(`secrets/${APP_ENV}.json`);
+const {
+  APP_ENV,
+  REDIS_PORT_6379_TCP_ADDR: REDIS_HOST = 'localhost',
+  C = true,
+  S = true,
+  R = false,
+} = process.env;
 
-if (require.main === module){
+const {
+  POSTGRES_HOST,
+  POSTGRES_USER,
+  POSTGRES_PASS,
+  POSTGRES_DB,
+  CONNECTWISE_CID,
+  CONNECTWISE_USER,
+  CONNECTWISE_PASS,
+} = require(`./secrets/${APP_ENV}.json`);
 
-    (function() {
+const redisInfo = {
+  port: 6379,
+  host: REDIS_HOST,
+};
 
-        const _context = require('repl').start({prompt: 'λ '}).context;
-        const scope = require('lexical-scope')(require('fs').readFileSync(__filename));
+const connect = { c: C, s: S, r: R };
 
-        for (var name in scope.locals[''] )
-            _context[scope.locals[''][name]] = eval(scope.locals[''][name]);
+const cwEnv = lo.includes([ 'test', 'development' ], APP_ENV) ? 'test' : 'production';
 
-        for (name in scope.globals.exported)
-            _context[scope.globals.exported[name]] = eval(scope.globals.exported[name]);
+Promise.all([
+  connect.c && postgresApi(
+    `postgres://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}/${POSTGRES_DB}`,
+    { logging: false }
+  ),
+  connect.s && connectwiseApi(CONNECTWISE_CID, CONNECTWISE_USER, CONNECTWISE_PASS, cwEnv),
+  connect.r && createClient(redisInfo.port, redisInfo.host),
+]).then(([ sequelize, connectwise, redis ]) => {
 
-    })();
+  // So that both short and long forms are available
+  const s = sequelize;
+  const c = connectwise;
+  const r = redis;
 
-}
+  const ex = (p) => p.then(console.log).catch(console.error);
+
+  const repl = require('repl').start({prompt: 'λ '});
+  const _context = repl.context;
+  const scope = require('lexical-scope')(fs.readFileSync(__filename));
+
+  for (var name in scope.locals[''] )
+    _context[scope.locals[''][name]] = eval(scope.locals[''][name]);
+
+  for (name in scope.globals.exported)
+    _context[scope.globals.exported[name]] = eval(scope.globals.exported[name]);
+
+  require('repl.history')(repl, './history/cw');
+
+});
